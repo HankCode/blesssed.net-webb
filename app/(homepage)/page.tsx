ee5e40012ba6dc102358b8c9324ad86936718361
Henrik Näsmark <61998706+HankCode@users.noreply.github.com>
2024-11-17 20:17:18 +0100

    Updates

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -5,7 +5,7 @@ export default async function Index() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <h2 className="font-medium text-xl mb-4">Imagemaker</h2>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam reiciendis aliquid
         blanditiis aspernatur dolores a totam amet, inventore quo corrupti minus libero atque
         nesciunt similique, excepturi debitis, dolorem sunt sit!
