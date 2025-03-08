2c7e83dc102f6a9e4bdf36e689fa429a85cf89ab
HankCode <henrik@naesmark.com>
2025-03-08 19:40:20 +0100

    Updates

--- app/(homepage)/_components/header.tsx
+++ app/(homepage)/_components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const HomeHeader = () => {
   return (
-    <nav className="w-full flex justify-center fixed z-40 h-16">
+    <nav className="w-full flex justify-center fixed z-40 h-16 bg-white">
       {/* <nav className="w-full flex justify-center fixed z-40 border-b border-b-foreground/10 h-16"> */}
       <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
         <div className="flex gap-5 items-center font-semibold">
